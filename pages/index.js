64fcc4c498b786f026e71e0c2cfc085ec9b133cf
Asha Balasubramaniam <asha.ab@gmail.com>
2024-08-23 23:25:45 +0530

    Remove unused home module styles

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Garden</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
+        <h1 className="text-4xl font-bold">
           Explore your Garden
         </h1>
         <img src="/banner.jpeg" alt="Garden Banner"/>
